8c29819438e73ed4e27592247089f79d757a7323
Rohit pal <rohitsinghpal17@gmail.com>
2025-04-04 17:04:04 +0530

    project completed

---
+++ Backend/src/lib/db.js
@@ -0,0 +1,19 @@
+import mongoose, { Mongoose } from "mongoose"
+
+
+export const connectDB = async () => {
+     try{
+          const conn=await mongoose.connect(process.env.MONGODB_URI);
+          console.log(`MongoDB connected ${conn.connection.host}`);
+
+     }
+     catch(error){
+          console.log("Mongodb error:",error)
+
+
+
+     }
+     
+          
+     }
+
---
+++ Backend/src/lib/socket.js
@@ -0,0 +1,37 @@
+import {Server} from 'socket.io'
+import http from 'http'
+import express from 'express'
+
+const app=express();
+const server =http.createServer(app);
+
+const io = new Server(server,{
+     cors:{
+          origin:["http://localhost:5173"]
+     }
+});
+export function getReceiverSocketId(userId){
+     return userSocketmap[userId];
+}
+
+//used to store online user
+     const userSocketmap ={};
+
+
+    io.on("connection",(socket)=>{
+     console.log("A user connected",socket.id);
+     const userId=socket.handshake.query.userId
+     if(userId) userSocketmap[userId]=socket.id
+//this is used to send events to all the connected clients
+     io.emit("getOnlineUsers",Object.keys(userSocketmap))
+
+     socket.on("disconnect",()=>{
+          console.log("A user disconnected",socket.id);
+          delete userSocketmap[userId];
+
+          io.emit("getOnlineUsers",Object.keys(userSocketmap))
+     });
+});
+
+
+export {io,app,server};
